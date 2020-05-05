f9eda9534efb779bb97b7c3f81cdc130e2b2d1c0
fsanchez-brujula <fsanchez@brujula.es>
2020-05-05 15:51:42 +0200

    app routing module

--- projects/my-lib/src/lib/app-routing/app-routing.module.ts
+++ projects/my-lib/src/lib/app-routing/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { Page1Component } from '../page1/page1.component';
 import { Page2Component } from '../page2/page2.component';
 
