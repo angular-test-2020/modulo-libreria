3e468119f8d00515a46a230590816aafa692fd90
fsanchez-brujula <fsanchez@brujula.es>
2020-05-05 10:57:46 +0200

    change version

--- projects/my-lib/src/lib/my-lib.component.ts
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -3,9 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'lib-my-lib',
   template: `
-    <p>
-      my-lib works!
-    </p>
+    <h2>
+      my-lib works v1.0.2!
+    </h2>
   `,
   styles: [
   ]
