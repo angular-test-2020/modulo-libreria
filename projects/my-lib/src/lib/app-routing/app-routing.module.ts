14f259afd7bf0ebca941f070b53a2f46e0fb569b
fsanchez-brujula <fsanchez@brujula.es>
2020-05-05 13:25:23 +0200

    routing

---
+++ projects/my-lib/src/lib/app-routing/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Page1Component } from '../page1/page1.component';
+import { Page2Component } from '../page2/page2.component';
+
+const routes: Routes = [
+  {path: '/page1', component: Page1Component},
+  {path: '/page2', component: Page2Component}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+/*@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})*/
+export class AppRoutingModule { }
--- projects/my-lib/src/lib/my-lib.module.ts
+++ projects/my-lib/src/lib/my-lib.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MyLibComponent } from './my-lib.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 
 
 @NgModule({
-  declarations: [MyLibComponent],
+  declarations: [MyLibComponent, Page1Component, Page2Component],
   imports: [
   ],
   exports: [MyLibComponent]
