2d4e4de4437977b2d6394c2be0779d4b528eb2f9
fsanchez-brujula <fsanchez@brujula.es>
2020-05-06 09:16:09 +0200

    data in routes

--- projects/my-lib/src/lib/app-routing/app-routing.module.ts
+++ projects/my-lib/src/lib/app-routing/app-routing.module.ts
@@ -4,8 +4,8 @@ import { Page1Component } from '../page1/page1.component';
 import { Page2Component } from '../page2/page2.component';
 
 const routes: Routes = [
-  {path: 'page1', component: Page1Component},
-  {path: 'page2', component: Page2Component}
+  {path: 'page1', component: Page1Component, data: {id: 'Liberia1'}},
+  {path: 'page2', component: Page2Component, data: {id: 'Liberia1'}}
 ];
 
 @NgModule({
